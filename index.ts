674f801ede07c93dfe8305e5efcda460a05105d9
Nimraakram12 <160039901+Nimraakram12@users.noreply.github.com>
2024-04-16 14:57:08 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n welcome to 'code with NIMRA' \n"))
+
+//define the list of currencies and their exchange rates
+
+let exchange_rate:any={
+    "USD":1,
+    "EUR":0.9,
+    "JYP":113,
+    "CAD":1.3,
+    "AUD":1.65,
+    "PKR":277.70,
+}
+
+//User yo select currency to convert
+
+let user_answer=await inquirer.prompt([{
+    name:"from_currency",
+    type:"list",
+    message:"select currency to convert from",
+    choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+
+},
+   {
+    name:"to_currency",
+    type:"list",
+    message:"select the currency to convert into",
+    choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+   },
+   {
+    name:"amount",
+    type:"input",
+    message:"enter the amount top convert",
+   }
+])
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount= exchange_rate[user_answer.to_currency];
+let amount= user_answer.amount
+
+
+//formula of conversion
+let base_amount = amount/from_amount
+let converted_amount = base_amount* to_amount
+
+
+//display the converted amount
+console.log(`converted_amount = ${converted_amount.toFixed(2)})`);
